10ae76226a45daf5b2c8e222f2543f42fa1c52c4
“lukemccrae” <lukemccrae@gmail.com>
2017-02-08 16:11:38 -0700

    passes test

---
+++ app.js
@@ -0,0 +1,11 @@
+function uniqueDigitProducts(arr) {
+  var result = [];
+  arr.forEach(function(number) {
+    
+  })
+  console.log(result);
+}
+uniqueDigitProducts([2, 8, 121, 42, 222, 23])
+
+
+// indexOf
